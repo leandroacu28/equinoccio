02e585f3b12ca32b7b163f27bfa595e70021658b
Leandro <findtech.sl@gmail.com>
2024-05-23 21:15:23 -0300

    integracion astro+preline

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        'preline': 'preline/dist/preline.js'
+      }
+    }
+  },
+
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}',
+		'node_modules/preline/dist/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		// require('@tailwindcss/forms'),
+		require('preline/plugin'),
+	],
 }
