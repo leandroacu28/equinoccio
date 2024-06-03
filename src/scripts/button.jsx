a843ec7aa689bbfa837d37a083ebe7e8f34cb836
Leandro <findtech.sl@gmail.com>
2024-06-02 21:54:39 -0300

    cambio de pc

--- src/scripts/button.jsx
+++ src/scripts/button.jsx
@@ -1,13 +1,19 @@
 
-  const button1= document.getElementById('button1');
-  const button2= document.getElementById('button2');
-
-  button1.addEventListener('click', () => {
-      button1.classList.add('hidden');
-      button2.classList.remove('hidden');
-  });
-
-  button2.addEventListener('click', () => {
-      button2.classList.add('hidden');
-      button1.classList.remove('hidden');
-  });
+        document.addEventListener('DOMContentLoaded', function() {
+          const button1 = document.getElementById('button1');
+          const button2 = document.getElementById('button2');
+    
+          if (button1 && button2) {
+            button1.addEventListener('click', () => {
+                button1.classList.add('hidden');
+                button2.classList.remove('hidden');
+            });
+    
+            button2.addEventListener('click', () => {
+                button2.classList.add('hidden');
+                button1.classList.remove('hidden');
+            });
+          } else {
+            console.error('One or both buttons are missing in the DOM.');
+          }
+        });
