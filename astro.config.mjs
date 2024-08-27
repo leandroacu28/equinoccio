6998c4652a4d5d41289da4d8166edcd2144649c9
Leandro <findtech.sl@gmail.com>
2024-08-27 13:09:48 -0300

    CAMBIO DE PC

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), preact()],
   vite: {
     resolve: {
       alias: {
         'preline': 'preline/dist/preline.js'
       }
     }
-  },
-
+  }
 });
---
+++ src/components/landing/MyReactComponent.jsx
@@ -0,0 +1,12 @@
+
+import react from 'react';
+
+function MyReactComponent() {
+  return (
+    <div>
+      <h2>Hello from React!</h2>
+    </div>
+  );
+}
+
+export default MyReactComponent;
